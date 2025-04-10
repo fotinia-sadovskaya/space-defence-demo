36468108d7c3e3f485af5bf3b921a09a4721c448
Svitlana <fotinia.sadovskaya@gmail.com>
2025-04-10 16:10:07 +0300

    Updated notify.js

--- public/utils/notify.js
+++ public/utils/notify.js
@@ -2,8 +2,14 @@ export function showToast(message = "✅ Готово", duration = 2000) {
   const toast = document.createElement("div");
   toast.className = "toast";
   toast.textContent = message;
+
+  // Додаємо toast до body
   document.body.appendChild(toast);
+
+  // Анімація появи
   setTimeout(() => toast.classList.add("toast--visible"), 10);
+
+  // Анімація зникнення
   setTimeout(() => {
     toast.classList.remove("toast--visible");
     setTimeout(() => toast.remove(), 300);
