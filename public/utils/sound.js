a908eeb751f5a043313ab40da9ae3e6e30f840c5
Svitlana <fotinia.sadovskaya@gmail.com>
2025-04-10 16:16:36 +0300

    Updated sound.js

--- public/utils/sound.js
+++ public/utils/sound.js
@@ -2,10 +2,20 @@ const sounds = {
   buy: new Audio("assets/sounds/buy.wav"),
   toggle: new Audio("assets/sounds/toggle.wav"),
 };
-export function playSound(name = "buy") {
+
+export function playSound(name) {
+  const store = JSON.parse(localStorage.getItem("spaceDefenceStore")) || {};
+  if (store.sound === false) return;
+
   const sound = sounds[name];
   if (sound) {
-    sound.volume = 0.5;
-    sound.play().catch((err) => console.warn("🔇 Звук не відтворено:", err));
+    sound.currentTime = 0; // Перезапуск звуку
+    sound.play().catch((err) => console.warn("🎧 Помилка звуку:", err));
   }
+}
+
+// Перемикач звуку — дивись store.js
+export function getSoundStatus() {
+  const store = JSON.parse(localStorage.getItem("spaceDefenceStore")) || {};
+  return store.sound !== false;
 }
